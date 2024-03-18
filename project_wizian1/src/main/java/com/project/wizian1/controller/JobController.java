4f10f3e14fd56528366baf6a79cae4fd3ddabe4b
alstkdwh24 <alstkdwh24@naver.com>
2024-03-18 13:59:54 +0900

    ㅇ

--- project_wizian1/src/main/java/com/project/wizian1/controller/JobController.java
+++ project_wizian1/src/main/java/com/project/wizian1/controller/JobController.java
@@ -1,17 +1,42 @@
 package com.project.wizian1.controller;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.project.wizian1.command.JobVO;
+import com.project.wizian1.job.service.JobService;
 
 @Controller
 @RequestMapping("/job")
 public class JobController {
-
+	
+	@Autowired
+	@Qualifier
+	private JobService jobService;
+	
+	
 	@GetMapping("/jobpost")
 	public String jobPost() {
 		
 		return "job/resume_jobpost";
 	}
-	
+	@PostMapping("/registForm")
+	public String regist(JobVO vo, RedirectAttributes ra) {
+		System.out.println(vo.toString());
+		int result = jobService.regist(vo);
+		if(result==1) {
+			ra.addFlashAttribute("msg","정상적으로 처리하였습니다");
+		}else {
+			ra.addFlashAttribute("msg","이상한 사람이다.");
+		}
+		return "redirect:/job/resume_jobpost";
+		
+	}
 }
--- project_wizian1/src/main/java/com/project/wizian1/job/service/JobServiceImpl.java
+++ project_wizian1/src/main/java/com/project/wizian1/job/service/JobServiceImpl.java
@@ -1,5 +1,17 @@
 package com.project.wizian1.job.service;
 
-public class JobServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.wizian1.command.JobVO;
+@Service("JobService")
+public class JobServiceImpl implements JobService{
+	@Autowired
+	private JobMapper jobMapper;
+	@Override
+	public int regist(JobVO vo) {
+		// TODO Auto-generated method stub
+		return jobMapper.regist(vo);
+	}
 
 }
