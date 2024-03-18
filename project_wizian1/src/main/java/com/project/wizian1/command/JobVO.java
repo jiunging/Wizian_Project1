bf96779810c0d2e1ca4b3e9484397e6d4141bb6a
alstkdwh24 <alstkdwh24@naver.com>
2024-03-18 13:59:54 +0900

    ㅇ

---
+++ project_wizian1/src/main/java/com/project/wizian1/command/JobVO.java
@@ -0,0 +1,33 @@
+package com.project.wizian1.command;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JobVO {
+	private String middle_content;
+	private String middle_content_catergory;
+	private String middle_middle_people;
+	private String middle_middle_writer;
+	private String middle_two_checkbox_two;
+	private String middle_two_select;
+	private String middle_two_content_content;
+	private String middle_two_checkbox_one;
+	private String middle_two_checkbox_two_two;
+	private String middle_two_checkbox_three;
+	private String middle_two_content_box;
+	private String middle_two_content_box_box;
+	private String middle_three_writer;
+	private String middle_three_content;
+	private String middle_three_content_email;
+	private String middle_three_content_select;
+	private String phone;
+	private String document;
+}
--- project_wizian1/src/main/java/com/project/wizian1/job/service/JobService.java
+++ project_wizian1/src/main/java/com/project/wizian1/job/service/JobService.java
@@ -1,5 +1,9 @@
 package com.project.wizian1.job.service;
 
+import com.project.wizian1.command.JobVO;
+
 public interface JobService {
+	
+	public int regist(JobVO vo);
 
 }
